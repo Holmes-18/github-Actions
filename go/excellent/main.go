50cde116caa46045a3d664f68b5994a60284c537
Holmes-18 <m.18.apple@gmail.com>
2026-05-05 10:14:39 +0900

    chore: コメント追加

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,5 +1,6 @@
 package main
 
+// コメント
 func EvenOrOdd(number int) string {
 	if number%2 == 0 {
 		return "even"
--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -2,9 +2,10 @@ package main
 
 import "testing"
 
+// コメント
 func TestEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
 	if result != "even" {
-		t.Errorf("expected: even, actual: %s, result")
+		t.Errorf("expected: even, actual: %s", result)
 	}
 }
